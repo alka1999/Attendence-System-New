81fd33e66f83776c050d440e71965defb38c2ad6
Sachin Kamble <sachin@clistesoft.com>
2021-12-22 23:30:35 +0300

    router done

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,43 +1,58 @@
-import React from 'react'
-import { Avatar } from 'primereact/avatar';
-import { Button } from 'primereact/button';
-import {  } from "../Landing/Landing.css";
-import { BrowserRouter, Link, Route } from "react-router-dom";
-import { Component } from 'react/cjs/react.development';
+import React from "react";
+import { Avatar } from "primereact/avatar";
+import { Button } from "primereact/button";
+import {} from "../Landing/Landing.css";
+import { Link } from "react-router-dom";
+import { Component } from "react/cjs/react.development";
 export default function Landing() {
-    
-    return (
-        <BrowserRouter>
-        <div>
-            <div className='mydiv text-center' >
-                <p>welcome to the <strong >HEXADIGI SOFTWARE PRIVATE LIMITED</strong> portal by.... </p>
-            </div>
-            <div className="p-grid">
-                <div className="p-col-8">
-                    <img src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frebeccaskilbeck%2Ffiles%2F2019%2F02%2FMaintianing-Motivation-in-employees-Blog-Graphic-1200x861.jpg" alt="img" height="650px" width="1000px" />
-                </div>
-                <div className="p-col-4">
-                    <div className='login'>
-                        <div className='profilediv p-p-0 p-p-sm-1 p-p-md-2 p-p-lg-1'>
-
-                            <Avatar image="https://writestylesonline.com/wp-content/uploads/2019/01/What-To-Wear-For-Your-Professional-Profile-Picture-or-Headshot.jpg" className="p-mr-2" size="large" shape="circle" />
-
-                        </div>
-                        <div className="demo-container p-p-0 p-p-sm-1 p-p-md-2 p-p-lg-3">
-                            <p> <b className='company'>HEXADIGI</b> Employess self service </p>
-                            <Avatar label="A" className="p-mr-2" size="xlarge" shape="circle" />
-                            
-                            <Button label="Login" className="p-button-rounded p-button-success loginbtn" />
-                            
-
-                            
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div>
+      <div className="mydiv text-center">
+        <p>
+          welcome to the <strong>HEXADIGI SOFTWARE PRIVATE LIMITED</strong>{" "}
+          portal by....{" "}
+        </p>
+      </div>
+      <div className="p-grid">
+        <div className="p-col-8">
+          <img
+            src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frebeccaskilbeck%2Ffiles%2F2019%2F02%2FMaintianing-Motivation-in-employees-Blog-Graphic-1200x861.jpg"
+            alt="img"
+            height="650px"
+            width="1000px"
+          />
         </div>
-        </BrowserRouter>
-    )
+        <div className="p-col-4">
+          <div className="login">
+            <div className="profilediv p-p-0 p-p-sm-1 p-p-md-2 p-p-lg-1">
+              <Avatar
+                image="https://writestylesonline.com/wp-content/uploads/2019/01/What-To-Wear-For-Your-Professional-Profile-Picture-or-Headshot.jpg"
+                className="p-mr-2"
+                size="large"
+                shape="circle"
+              />
+            </div>
+            <div className="demo-container p-p-0 p-p-sm-1 p-p-md-2 p-p-lg-3">
+              <p>
+                {" "}
+                <b className="company">HEXADIGI</b> Employess self service{" "}
+              </p>
+              <Avatar
+                label="A"
+                className="p-mr-2"
+                size="xlarge"
+                shape="circle"
+              />
+
+              <Link to="/login">
+                <Button className="p-button-rounded p-button-success loginbtn">
+                  Login
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,44 +1,58 @@
-import React from 'react'
-import { InputText } from 'primereact/inputtext';
-import { Checkbox } from 'primereact/checkbox';
-import { Button } from 'primereact/button';
-import { } from "../Login/Login.css";
+import React from "react";
+import { InputText } from "primereact/inputtext";
+import { Checkbox } from "primereact/checkbox";
+import { Button } from "primereact/button";
+import {} from "../Login/Login.css";
+import { Link } from "react-router-dom";
 export default function Login() {
-    return (
-        <div>
-            <form className='login-form'>
-                <div className='company-name p-p-0 p-p-sm-1 p-p-md-2 p-p-lg-1'>
-                    <h2>HEXADIGI SOFTWARE PRIVATE LIMITED</h2>
-                </div>
-                <div className='inputs'>
-                    <div className="p-fluid">
-                        <div className="p-field">
-                            <InputText className='input' id="firstname1" type="text" placeholder='Enter Username' />
-                        </div>
-                        <div className="p-field">
-                            <InputText className='input' id="lastname1" type="text" placeholder='Enter Password' />
-                        </div>
-                    </div>
-                    <div className='p-grid'>
-                        <div className='p-col checkboxcol'>
-
-                            <div className="p-field-checkbox">
-                                <InputText className='input' inputId="city1" type='checkbox' name="city"/>
-                                <label htmlFor="city1">Remember me</label>
-
-                            </div>
-
-                            
-                        </div>
-                        <div className='p-col'>
-                            <Button label="Login" className="p-button-success btnl" />
-                            </div>
-                    </div>
-                    <div className='forpass'>
-                        <a href="">Forget password</a>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div>
+      <form className="login-form">
+        <div className="company-name p-p-0 p-p-sm-1 p-p-md-2 p-p-lg-1">
+          <h2>HEXADIGI SOFTWARE PRIVATE LIMITED</h2>
         </div>
-    )
+        <div className="inputs">
+          <div className="p-fluid">
+            <div className="p-field">
+              <InputText
+                className="input"
+                id="firstname1"
+                type="text"
+                placeholder="Enter Username"
+              />
+            </div>
+            <div className="p-field">
+              <InputText
+                className="input"
+                id="lastname1"
+                type="text"
+                placeholder="Enter Password"
+              />
+            </div>
+          </div>
+          <div className="p-grid">
+            <div className="p-col checkboxcol">
+              <div className="p-field-checkbox">
+                <InputText
+                  className="input"
+                  inputId="city1"
+                  type="checkbox"
+                  name="city"
+                />
+                <label htmlFor="city1">Remember me</label>
+              </div>
+            </div>
+            <div className="p-col">
+              <Link to="/layout">
+                <Button className="p-button-success btnl">Login</Button>
+              </Link>
+            </div>
+          </div>
+          <div className="forpass">
+            <a href="">Forget password</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
